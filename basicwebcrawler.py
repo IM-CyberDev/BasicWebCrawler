3971fbf3364f03f0e10b515b8d87904070bc8f39
Ikram123 <ikram.makhloufi@etu.u-pec.fr>
2020-01-09 22:01:13 +0100

    switched to python3

--- basicwebcrawler.py
+++ basicwebcrawler.py
@@ -1,10 +1,10 @@
 import requests
 import re
 
-global a = []
-global url_starter = ""#change to your url here, searching only url with .com at the end, change it to what you want. Or include a pattern list
-global url_domainname = None
-global temp = re.search(r"\.([^ \.]+)\.com",url_starter)
+a = []
+url_starter = ""#change to your url here, searching only urls with .com at the end, change it to what you want. Or include a pattern list
+url_domainname = None
+temp = re.search(r"\.([^ \.]+)\.com",url_starter)
 if temp:
 	url_domainname = temp.group(1)
 
@@ -12,43 +12,30 @@ def web_crawl(weburl):
 	url = weburl
 	page = requests.get(url)
 	webpage = page.text
-	xx = re.findall(r"href=\"([^ ]+)\"",webpage)
-	if xx:
-		s = "\n".join(xx)
+	newLinks = re.findall(r"href=\"([^ ]+)\"",webpage)
+	if newLinks:
+		s = "\n".join(newLinks)
 		print (s)
 	
 	
-	if xx == None:
+	if newLinks == None:
 		return
 	a.append(url)
 	
-	for newp in xx:
-		if "http" in newp:
-			s = re.search(r"\.([^ \.]+)\.com",newp)
-			if s.group(1) in url and newp not in a and s.group(1)==url_domainname:
-				print newp
-				web_crawl(newp)
-		else:
-			s = url+"/"+newp
-			if s != url and ".." not in s:
-				print s
-				web_crawl(s)
-				
-	
-	for newp in xx:
-		if "http" in newp:
-			s = re.search(r"\.([^ \.]+)\.com",newp)
-			if s.group(1) in url and url!=newp and s.group(1)!=url_domainname:
-				print newp
-				web_crawl(newp)
-		else:
-			s = url+"/"+newp
-			if s != url and ".." not in s:
-				print s
-				web_crawl(s)
-		
+	for newp in newLinks:
+		try:
+			if "http" in newp:
+				s = re.search(r"\.([^ \.]+)\.com",newp)
+				if s.group(1) in url and newp not in a and s.group(1)==url_domainname and url!=newp:
+					print (newp)
+					web_crawl(newp)
+			else:
+				s = url+"/"+newp
+				if s != url and ".." not in s:
+					print (s)
+					web_crawl(s)
+		except:
+			continue
 	
 	
-	
-
 web_crawl(url_starter)
